de9a3fcde28eadac7d24e98aebf50b600be25d0f
Sujay Bajracharya <sbajrach@andrew.cmu.edu>
2020-01-25 19:45:02 -0500

    testing changes unstable

--- src/sawyer_control/envs/sawyer_slide_simple.py
+++ src/sawyer_control/envs/sawyer_slide_simple.py
@@ -12,7 +12,7 @@ class SawyerSlideEnv(SawyerEnvBase):
 
 	def __init__(self,
 				 fix_goal=True,
-				 fixed_goal=(1,0,0),
+				 fixed_goal=(0.53529716, 0.16153553, 0.37543553),
 				 action_mode='position',
 				 goal_low=None,
 				 goal_high=None,
@@ -23,13 +23,20 @@ class SawyerSlideEnv(SawyerEnvBase):
 		SawyerEnvBase.__init__(self, action_mode=action_mode, **kwargs)
 		self.tactile = rospy.ServiceProxy('tactile_service', tactile, persistent=False)
 		self.gripper_pos = rospy.ServiceProxy('/wsg_50_driver/move', Move, persistent=False)
-		self.gripper_pos_scale = 50
+		self.gripper_pos_scale = 10
 
 		# TODO: tune these upper and lower thresholds
 		self.force_lower = np.array([0, 0, 0])
 		self.force_upper = np.array([100, 100, 100])
 
+		#self.start_pose = (0.50417823,-0.25704312,0.36392656)
+		#self.start_pose = (np.array([-0.68556154, -1.12045896, -3.04373741, -1.76745212, -0.05429785, -0.89508206, -3.61514544]), np.array([ 0.61650902, -0.15921682,  0.30698016]))
+		self.start_pose = (np.array([ 0.35342872, -0.65991211, -0.82064646,  1.42971587, -2.43175673,
+       -1.10287106, -3.65176177]), np.array([ 0.49755925, -0.12712517,  0.34312445]))
+
 		self.goal_dir = fixed_goal
+		_,_,eepos = self.request_observation()
+		print(eepos)
 
 
 	def _set_observation_space(self):
@@ -48,7 +55,22 @@ class SawyerSlideEnv(SawyerEnvBase):
 	def _act(self, action):
 		if self.action_mode == 'position':
 			self.set_gripper_pos(action * self.gripper_pos_scale)
-			#self._position_act(self.goal_dir - self._get_endeffector_pose())
+			#self._position_act((self.goal_dir - self._get_endeffector_pose()[:3])*self.position_action_scale)
+			self._position_act(np.array([0,0,1])*self.position_action_scale)
+
+
+	def _position_act(self, action):
+		ee_pos = self._get_endeffector_pose()
+		endeffector_pos = ee_pos[:3]
+		target_ee_pos = (endeffector_pos + action)
+		target_ee_pos = np.clip(target_ee_pos, self.config.POSITION_SAFETY_BOX_LOWS, self.config.POSITION_SAFETY_BOX_HIGHS)
+		#orientation = [0.72693193, 0., -0.72693193, 0.]
+		#orientation = [-0.6842742, 0.7283662, -0.02291047, 0.02695494]
+		orientation = ee_pos[3:]
+		#target_ee_pos = np.concatenate((target_ee_pos, orientation))
+		target_ee_pos = np.concatenate((target_ee_pos, orientation))
+		angles = self.request_ik_angles(target_ee_pos, self._get_joint_angles())
+		self.send_angle_action(angles, target_ee_pos)
 
 
 	def _get_obs(self):
@@ -104,13 +126,12 @@ class SawyerSlideEnv(SawyerEnvBase):
 	# TODO: test custom ROS functions
 	def _reset_robot(self):
 		if not self.reset_free:
-			#self.open_gripper()
+			print("resetting")
+			self.set_gripper_pos(10)
 			#for _ in range(5):
-				#self._position_act(self.pos_control_reset_position - self._get_endeffector_pose())
+			#	self._position_act(self.start_pose - self._get_endeffector_pose())
+			self.set_env_state(self.start_pose)
 			self.set_gripper_pos(0)                
-
-	def open_gripper(self):
-		self.set_gripper_pos(50.0)
 	
 	def set_gripper_pos(self, pos):
 		self.gripper_pos(float(pos), 70.0)
